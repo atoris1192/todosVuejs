86dcdac5a16efcd6279a4c15b10cdff6d52e7366
atoris1192 <atoris1192@gmail.com>
2019-09-14 00:54:01 +0900

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Vue.js で todo 
 * parcel src/index.pug
 localStorageを使用
+* https://jp.vuejs.org/v2/examples/todomvc.html
+* Vue.js　チュートリアル
--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -207,7 +207,7 @@ new Vue({
     }
   }
 });
-},{}],"../../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -234,7 +234,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64889" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55718" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
@@ -376,5 +376,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},["../../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","main.js"], null)
+},{}]},{},["../../../../../.config/yarn/global/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","main.js"], null)
 //# sourceMappingURL=/main.1f19ae8e.map
